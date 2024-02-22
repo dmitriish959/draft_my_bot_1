d9f5320de04b26d18b5bc4fcdbd5ade188451b64
dmitriish959 <80873437+dmitriish959@users.noreply.github.com>
2024-02-23 00:18:37 +0500

    попытка создать БД для ввода в нее регистрационных данных

--- main.py
+++ main.py
@@ -1,22 +1,39 @@
+import sqlite3
+
 import telebot
 # импорт токена
 from env import MY_TOKEN
+
 # передача токена
 bot = telebot.TeleBot(MY_TOKEN)
-#Обработчик отвечающий на команду старт
+# Обработчик отвечающий на команду старт
 bot.message_handler(commands=['start'])
-#Функция спрашивающая имя
+
+
+# Функция спрашивающая имя
 def start(message):
-    sent = bot.send_message(message.chat.id, 'Как тебя завут?')
-    bot.register_next_step_handler(sent, hello)
+    # создание БД
+    conn = sqlite3.connect('registr.sql')
+    cur = conn.cursor()
+
+    cur.execute('CREATE TABLE IF NOT EXISTS user(id int auto_increment primory key, name varchar(80)')
+    conn.commit()
+    cur.close()
+    conn.close()
+
+    bot.send_message(message.chat.id, 'Привет')
+    bot.register_next_step_handler(message, user_name)
+    # cur.execute('')
+    # sent = bot.send_message(message.chat.id, 'Как тебя завут?')
+    # bot.register_next_step_handler(sent, hello)
+
 
 def hello(message):
-    bot.send_message(message.chat.id, 'Привет, {name}. Рад тебя видеть.'. format(name=message.text))
+    bot.send_message(message.chat.id, 'Привет, {name}. Рад тебя видеть.'.format(name=message.text))
 
-def cars(message):
-    sent.bot_message(message.chat.id, 'Выберите марку ТС')
+
+# def cars(message):
+#    sent.bot_message(message.chat.id, 'Выберите марку ТС')
 
 # Функция polling заставляет нашего бота постоянно контачить с ТГ
 bot.polling()
-
-
