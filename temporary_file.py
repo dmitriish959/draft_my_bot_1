74db6641a91264f6112383303c4a60775b28af24
dmitriish959 <80873437+dmitriish959@users.noreply.github.com>
2024-02-25 20:20:24 +0500

    Добавление имени в базу данных

--- temporary_file.py
+++ temporary_file.py
@@ -18,33 +18,3 @@ user.save()
 
 # Закрываем соединение с базой данных
 db.close()
-
-class User(Model):
-    telephone = IntegerField()
-
-    class Meta:
-        database = db
-db.connect()
-
-# Создаем новую запись пользователя с именем 'Alice'
-user = User(telephone= '+79821879991')
-user.save()
-
-# Закрываем соединение с базой данных
-db.close()
-
-class User(Model):
-    city = CharField()
-
-    class Meta:
-        database = db
-
-
-db.connect()
-
-# Создаем новую запись пользователя с именем 'Alice'
-user = User(city= 'US')
-user.save()
-
-# Закрываем соединение с базой данных
-db.close()
